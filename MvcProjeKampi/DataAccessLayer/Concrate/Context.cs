3b6c477e775a8636776d816874134e8b96dd1407
serhat <serhat.sural@ericssonmsp.com>
2021-04-23 14:20:07 +0300

    1/4 first commit
    1/4 first commit

---
+++ MvcProjeKampi/DataAccessLayer/Concrate/Context.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrate
+{
+    public class Context:DbContext
+    {   //buradakiler sqle bir tablo olarak yansıtılacak
+        //sınıf ismi-sql deki tablonun ismi
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Content> Contents { get; set; }
+        public DbSet<Heading> Headings { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+    }
+}
